c5a211f70e9fba8bc18013a17b2f3d61834c77d9
Lv Quý <quyseo.ictu@gmail.com>
2017-10-09 16:37:12 +0700

    Add files via upload

--- ktnamnhuan.py
+++ ktnamnhuan.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
-class ktnhuan():
-    def __init__(self,nam):
-        self.nam = nam
-    def ktnam(self):
-        if self.nam % 4 == 0 and self.nam % 100 != 0 or (self.nam % 400 == 0 and self.nam % 100 != 0):
-            return 'Nhuận'
-        else:
-           return 'Không nhuận'
-i = 1
-while i == 1:
-    a = ktnhuan(input('nhập năm:'))
-    print a.ktnam()
-    i = input('Nhập 1 để tiếp tục: ')
-else:
-    print 'Xin chào'
+def kt(nam):
+    if nam % 19 == 0 or nam % 19 ==  3 or nam % 19 == 6 or nam % 19 ==  9 or nam % 19 ==  11 or nam % 19 ==  14 or nam % 19 ==  17 :
+        return True
+    return False
+print kt(input('Nhập năm cần kt: '))
