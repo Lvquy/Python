4b54e488e6aa0211e8e92eb34fa8fe0bba2fd4f7
Lvquy <quyseo.ictu@gmail.com>
2017-10-09 13:14:43 +0700

    Add files via upload

--- kiem tra so nguyen to/finsongto.py
+++ kiem tra so nguyen to/finsongto.py
@@ -5,10 +5,13 @@ a = input('Nhập số đầu vào: ')
 if a ==1:
     print 'Không có số nguyên tố nào trong khoảng này'
 if a == 2:
-    songto.append(2)
-if a > 2:
+    print 'Các số nguyên tố từ 1 tới 2: [2]'
+    print 'Từ 1 tới 2 có 1 số nguyên tố'
+if 2 <a< 99999:
     for i in range(2, a+1):
         if ktsonguyento.kt(i) == True:
             songto.append(i)
-print 'Các số nguyên tố từ 1 tới',a,':',songto
-print 'Từ 1 tới',a,'có',songto.__len__(),'số nguyên tố'
+    print 'Các số nguyên tố từ 1 tới', a, ':', songto
+    print 'Từ 1 tới', a, 'có', songto.__len__(), 'số nguyên tố'
+if a> 99998:
+    print 'Nhập số bé hơn 99999'
--- kiem tra so nguyen to/ktsonguyento.py
+++ kiem tra so nguyen to/ktsonguyento.py
@@ -1,10 +1,13 @@
 def kt(so):
+
     if so == 1:
         return False
     if so == 2:
         return True
-    if so >2:
+    if 2<so<99999:
         for i in range(2,int(so*0.5)+1):
             if so % i ==0:
                 return False
         return True
+    else:
+        return None
